e2ed886e4486b16a1a433f4bb75fb06b4070b714
Brice Kamhoua <kamhoua.k.brice@gmail.com>
2023-12-28 12:01:53 +0100

    format

--- cx.go
+++ cx.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+  "fmt"
+  "log"
+  "os"
 
-	"github.com/bricekams/cx/db"
-	"github.com/urfave/cli/v2"
+  "github.com/bricekams/cx/db"
+  "github.com/urfave/cli/v2"
 )
 
 func main() {
-	log.SetFlags(0)
-	log.SetPrefix("cx: ")
-	db.Init()
-	app := &cli.App{
-		Name:    "cxdir",
-		Version: "v0.1",
-		Authors: []*cli.Author{
-			{
-				Name:  "Brice Kamhoua",
-				Email: "kamhoua.k.brice@gmail.com",
-			},
-		},
-		Copyright: "(c) 2023 Brice Kamhoua",
-		Usage:     "A simple shortcuts manager",
-		// UsageText: "cx [global options] command [command options] [arguments...]",
-		Commands: []*cli.Command{
+  log.SetFlags(0)
+  log.SetPrefix("cx: ")
+  db.Init()
+  app := &cli.App{
+    Name:    "cxdir",
+    Version: "v0.1",
+    Authors: []*cli.Author{
+      {
+        Name:  "Brice Kamhoua",
+        Email: "kamhoua.k.brice@gmail.com",
+      },
+    },
+    Copyright: "(c) 2023 Brice Kamhoua",
+    Usage:     "A simple shortcuts manager",
+    // UsageText: "cx [global options] command [command options] [arguments...]",
+    Commands: []*cli.Command{
 
-			{
-				Name:    "create",
-				Aliases: []string{"c"},
-				Usage:   "Create a new shortcut",
-				Before: func(cCtx *cli.Context) error {
-					name := cCtx.Args().First()
-					if name == "" {
-						log.Fatal("Please provide a shortcut name")
-					}
-					err := validateName(name)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					return nil
-				},
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "path", Usage: "Specify the path to be saved", Aliases: []string{"p"}},
-				},
-				Action: func(cCtx *cli.Context) error {
-					name := cCtx.Args().First()
-					path := cCtx.String("path")
+      {
+        Name:    "create",
+        Aliases: []string{"c"},
+        Usage:   "Create a new shortcut",
+        Before: func(cCtx *cli.Context) error {
+          name := cCtx.Args().First()
+          if name == "" {
+            log.Fatal("Please provide a shortcut name")
+          }
+          err := validateName(name)
+          if err != nil {
+            log.Fatal(err.Error())
+          }
+          return nil
+        },
+        Flags: []cli.Flag{
+          &cli.StringFlag{Name: "path", Usage: "Specify the path to be saved", Aliases: []string{"p"}},
+        },
+        Action: func(cCtx *cli.Context) error {
+          name := cCtx.Args().First()
+          path := cCtx.String("path")
           var err_path error = nil
           if path == "" {
             path, err_path = os.Getwd()
@@ -55,31 +55,31 @@ func main() {
               log.Fatal("An error occurred while processing the path")
             }
           }
-					exists, err_exists := exists(path)
-					if err_exists != nil {
-						log.Fatal("An error occured while processing the path")
-					}
-					if !exists {
+          exists, err_exists := exists(path)
+          if err_exists != nil {
+            log.Fatal("An error occured while processing the path")
+          }
+          if !exists {
             log.Println(path)
-						log.Fatal("The given path does not exists")
-					}
-					rootedPath, err_rootedPath := resolvePath(path)
-					if err_rootedPath != nil {
-						log.Fatal("An error occured while resolving the path")
-					}
-					err := db.Create(name, rootedPath)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					log.Printf("Shortcut created successfully. Use `cx %s`\n", name)
-					return nil
-				},
-			},
+            log.Fatal("The given path does not exists")
+          }
+          rootedPath, err_rootedPath := resolvePath(path)
+          if err_rootedPath != nil {
+            log.Fatal("An error occured while resolving the path")
+          }
+          err := db.Create(name, rootedPath)
+          if err != nil {
+            log.Fatal(err.Error())
+          }
+          log.Printf("Shortcut created successfully. Use `cx %s`\n", name)
+          return nil
+        },
+      },
 
-			{
-				Name:    "update",
-				Aliases: []string{"u"},
-				Usage:   "Update a shortcut path",
+      {
+        Name:    "update",
+        Aliases: []string{"u"},
+        Usage:   "Update a shortcut path",
         Before: func(cCtx *cli.Context) error {
           name := cCtx.Args().First()
           if name == "" {
@@ -120,43 +120,43 @@ func main() {
         },
       },
 
-			{
-				Name:      "rename",
-				Aliases:   []string{"r"},
-				Usage:     "rename a shortcut",
-				UsageText: "cx rename <old_name> <new_name>",
-				Before: func(cCtx *cli.Context) error {
-					oldName := cCtx.Args().First()
-					newName := cCtx.Args().Get(1)
-					if oldName == "" || newName == "" {
-						log.Fatal("rename expect two args. see --help")
-					}
-					err := validateName(oldName)
-					err_n := validateName(newName)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					if err_n != nil {
-						log.Fatal(err_n.Error())
-					}
-					return nil
-				},
-				Action: func(cCtx *cli.Context) error {
-					oldName := cCtx.Args().First()
-					newName := cCtx.Args().Get(1)
-					err := db.Rename(oldName, newName)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					log.Printf("Shortcut renamed successfully. Use `cx %s`\n",newName)
-					return nil
-				},
-			},
+      {
+        Name:      "rename",
+        Aliases:   []string{"r"},
+        Usage:     "rename a shortcut",
+        UsageText: "cx rename <old_name> <new_name>",
+        Before: func(cCtx *cli.Context) error {
+          oldName := cCtx.Args().First()
+          newName := cCtx.Args().Get(1)
+          if oldName == "" || newName == "" {
+            log.Fatal("rename expect two args. see --help")
+          }
+          err := validateName(oldName)
+          err_n := validateName(newName)
+          if err != nil {
+            log.Fatal(err.Error())
+          }
+          if err_n != nil {
+            log.Fatal(err_n.Error())
+          }
+          return nil
+        },
+        Action: func(cCtx *cli.Context) error {
+          oldName := cCtx.Args().First()
+          newName := cCtx.Args().Get(1)
+          err := db.Rename(oldName, newName)
+          if err != nil {
+            log.Fatal(err.Error())
+          }
+          log.Printf("Shortcut renamed successfully. Use `cx %s`\n",newName)
+          return nil
+        },
+      },
 
-			{
-				Name:    "delete",
-				Aliases: []string{"d"},
-				Usage:   "Delete a shortcut",
+      {
+        Name:    "delete",
+        Aliases: []string{"d"},
+        Usage:   "Delete a shortcut",
         Before: func(cCtx *cli.Context) error {
           name := cCtx.Args().First()
           if name == "" {
@@ -167,16 +167,16 @@ func main() {
         Flags: []cli.Flag{
           &cli.BoolFlag{Name: "all", Aliases: []string{"a"}, Usage: "Delete all the saved shortcuts"},
         },
-				Action: func(cCtx *cli.Context) error {
-					name := cCtx.Args().First()
+        Action: func(cCtx *cli.Context) error {
+          name := cCtx.Args().First()
           err := db.Delete(name)
           if err!=nil {
             log.Fatal(err.Error())
           }
           log.Println("Shortcut", name, "has been deleted succesfully")
-					return nil
-				},
-			},
+          return nil
+        },
+      },
 
       {
         Name: "list",
@@ -186,10 +186,10 @@ func main() {
           return nil
         },
       },
-		},
-	}
-	app.Suggest = true
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+    },
+  }
+  app.Suggest = true
+  if err := app.Run(os.Args); err != nil {
+    log.Fatal(err)
+  }
 }
